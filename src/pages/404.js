59db8cce600d73f14b3468f817a49423c2a123a5
Peter Witham <peter.witham@gmail.com>
2019-02-17 02:19:33 -0600

    Updated 404 with picture.

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import {Cat} from 'react-kawaii'
 
 const NotFoundPage = () => (
   <Layout>
       <br/><br/>
     <div className="error-message">
+        <Cat size={320} mood="sad" color="#596881"/>
       <h1>NOT FOUND</h1><br/>
       <p>Ooops! that doesn&#39;t exist...</p><br/>
         <a href={"/"}>Click here to visit the home page
