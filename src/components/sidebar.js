dc4b6766eba2da360967ef7a261498d0699a8ba6
Peter Witham <peter.witham@gmail.com>
2019-02-26 12:23:28 -0600

    Fixed the font size on the sidebar.

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -9,25 +9,28 @@ import githubIcon from '../img/github.svg'
 import mailIcon from '../img/mail.svg'
 
 const Sidebar = () => (
-    <div className="sidebar">
-        <Aboutbox/>
-        <h2 class="sidebarWidget">Navigation</h2>
-        <a href={'/tags/swift'}><img src={swiftIcon} height="20px" width="18px"/> Swift Programming</a><br/>
-        <a href={'/tags/podcast'}><img src={tlcPodcastIcon} height="20px" width="20px"/> Listen to Podcasts</a><br/>
-            {/*<a href={'./tags/jamstack'}>Learn JAMStack</a><br/>
-        <a href={'./tags/videos'}>Watch Videos</a><br/>*/}
-        <br/>
-        <h2 class="sidebarWidget">Social</h2>
-        <a href={'/contact/'}><img src={mailIcon} width="20px" height="20px"/> Contact Me</a><br/>
-        <a href={'https://twitter.com/compileswift'} target='_blank' rel="noopener noreferrer">
-                <img src={twitterIcon} width="20px" height="20px"/> Twitter</a><br/>
-        <a href={'https://www.linkedin.com/in/peterwitham'} target='_blank' rel="noopener noreferrer">
-                <img src={linkedInIcon} width="20px" height="20px" /> LinkedIN</a><br/>
-        <a href={'https://github.com/GrfxGuru'} target='_blank' rel="noopener noreferrer">
-                <img src={githubIcon} width="20px" height="20px"/> GitHub</a><br/>
-        <br/>
-        <BuyMeACoffee/>
-    </div>
+  <div className='sidebar'>
+    <Aboutbox/>
+    <h2 class='sidebarWidget'>Navigation</h2>
+    <a href={'/tags/swift'}><img src={swiftIcon} height='20px' width='18px' /> Swift Programming</a>
+    <br/>
+    <a href={'/tags/podcast'}><img src={tlcPodcastIcon} height='20px' width='20px' /> Listen to Podcasts</a>
+    <br/>
+    {/*<a href={'./tags/jamstack'}>Learn JAMStack</a><br/>
+            <a href={'./tags/videos'}>Watch Videos</a><br/>*/}
+    <br/>
+    <h2 class='sidebarWidget'>Social</h2>
+    <a href={'/contact/'}><img src={mailIcon} width='20px' height='20px' /> Contact Me</a>
+    <br/>
+    <a href={'https://twitter.com/compileswift'} target='_blank' rel='noopener noreferrer'><img src={twitterIcon} width='20px' height='20px' /> Twitter</a>
+    <br/>
+    <a href={'https://www.linkedin.com/in/peterwitham'} target='_blank' rel='noopener noreferrer'><img src={linkedInIcon} width='20px' height='20px' /> LinkedIN</a>
+    <br/>
+    <a href={'https://github.com/GrfxGuru'} target='_blank' rel='noopener noreferrer'><img src={githubIcon} width='20px' height='20px' /> GitHub</a>
+    <br/>
+    <br/>
+    <BuyMeACoffee/>
+  </div>
 )
 
 export default Sidebar
