4a4db6d6509b1430dcc95cc0aba5a8f636e93c1c
Peter Witham <peter.witham@gmail.com>
2019-03-13 01:13:25 -0500

    Updated the about component.

--- src/components/aboutbox.js
+++ src/components/aboutbox.js
@@ -3,7 +3,8 @@ import React from 'react'
 const Aboutbox = () => (
     <div className={'sidebarabout'}>
         <h2 className={'sidebarWidget'}>Who I Am</h2>
-        <p>I'm a software developer and artist. I live and breath creativity and knowledge sharing.</p>
+        <p>I'm a lead Web and Mobile software developer, artist and sometimes Podcaster. I live and breath creativity
+            and knowledge sharing.</p>
     </div>
 )
 
