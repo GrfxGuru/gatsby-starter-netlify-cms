9f2bc0fc8335ac376d16e8744dec8bee681b15d8
Peter Witham <peter.witham@gmail.com>
2019-07-28 18:16:59 -0500

    Updated links for TLC podcast.

--- src/components/podcast_tlc_links.js
+++ src/components/podcast_tlc_links.js
@@ -17,30 +17,30 @@ const PodcastLinksTLC = () => (
                 <img src={require('../img/podcast_players/pocket_casts.png')} alt="Pocket Casts icon" width="32"
                      height="32"/>
             </a>
-            <a href={"https://overcast.fm/itunes1269813811/the-life-creative"}>
+            <a href={"https://play.radiopublic.com/the-life-creative-WYBnel"}>
                 <img src={require('../img/podcast_players/radiopublic.png')} alt="Radio Public icon" width="32"
                      height="32"/>
             </a>
-            <a href={"https://overcast.fm/itunes1269813811/the-life-creative"}>
+            <a href={"https://podcasts.apple.com/us/podcast/the-life-creative/id1269813811"}>
                 <img src={require('../img/podcast_players/apple_podcasts.png')} alt="Apple icon" width="32"
                      height="32"/>
             </a>
-            <a href={"https://overcast.fm/itunes1269813811/the-life-creative"}>
+            <a href={"https://open.spotify.com/show/1hd27peL8JEoy4WMJq33Un"}>
                 <img src={require('../img/podcast_players/spotify.png')} alt="Spotify icon" width="32" height="32"/>
             </a>
-            <a href={"https://overcast.fm/itunes1269813811/the-life-creative"}>
+            <a href={"https://playmusic.app.goo.gl/?ibi=com.google.PlayMusic&isi=691797987&ius=googleplaymusic&link=https://play.google.com/music/m/Idhzktukhazvz55gayca5mdfocy?t%3DThe_Life_Creative%26pcampaignid%3DMKT-na-a"}>
                 <img src={require('../img/podcast_players/google_play.png')} alt="Google Play icon" width="32"
                      height="32"/>
             </a>
-            <a href={"https://overcast.fm/itunes1269813811/the-life-creative"}>
+            <a href={"https://www.google.com/podcasts?feed=aHR0cHM6Ly9hbmNob3IuZm0vcy85YTJmMTgvcG9kY2FzdC9yc3M="}>
                 <img src={require('../img/podcast_players/google_podcasts.png')} alt="Google Podcasts icon" width="32"
                      height="32"/>
             </a>
-            <a href={"https://overcast.fm/itunes1269813811/the-life-creative"}>
+            <a href={"https://castbox.fm/channel/id1015404"}>
                 <img src={require('../img/podcast_players/castbox.png')} alt="Castbox player icon" width="32"
                      height="32"/>
             </a>
-            <a href={"https://overcast.fm/itunes1269813811/the-life-creative"}>
+            <a href={"https://www.breaker.audio/the-life-creative"}>
                 <img src={require('../img/podcast_players/breaker.png')} alt="Breaker player icon" width="32"
                      height="32"/>
             </a>
