a1d5d46f04e8f88c80285a2e0c2d87454aab27b9
Peter Witham <peter.witham@gmail.com>
2019-07-28 18:04:35 -0500

    Updated links for TLC podcast players, added CompileSwift podcast template and post.

--- src/components/podcast_cs_links.js
+++ src/components/podcast_cs_links.js
@@ -1,46 +1,38 @@
 import React from 'react'
 
-const PodcastLinksTLC = () => (
+const PodcastLinksCS = () => (
     <div className="podcastPlayersLinkTable">
         <h2>Subscribe to the Podcast</h2>
-        <p>If you like this episode, please check out others at <a href="https://pw.d.pr/NBc0dv" target="_blank"
-                                                                   rel="noopener noreferrer"> The Life Creative
-            Podcast</a>. Or click the link to your favorite Podcast player.</p>
+        <p>If you like this episode, please check out others at <a href="https://pw.d.pr/5TbjRs" target="_blank"
+                                                                   rel="noopener noreferrer">CompileSwift Podcast</a>.
+            Or click the link to your favorite Podcast player.</p>
         <div className="podcastPlayerIconList">
-            <a href={"https://overcast.fm/itunes1269813811/the-life-creative"}>
+            <a href={"https://overcast.fm/itunes1467065787/compile-swift"}>
                 <img src={require('../img/podcast_players/overcast.png')} alt="Overcast icon" width="32" height="32"/>
             </a>
-            <a href={"https://pw.d.pr/NBc0dv"}>
+            <a href={"https://pw.d.pr/5TbjRs"}>
                 <img src={require('../img/podcast_players/anchor.png')} alt="Anchor icon" width="32" height="32"/>
             </a>
             <a href={"http://pca.st/uEB6"}>
                 <img src={require('../img/podcast_players/pocket_casts.png')} alt="Pocket Casts icon" width="32"
                      height="32"/>
             </a>
-            <a href={"https://overcast.fm/itunes1269813811/the-life-creative"}>
+            <a href={"https://radiopublic.com/compile-swift-G7VbpB"}>
                 <img src={require('../img/podcast_players/radiopublic.png')} alt="Radio Public icon" width="32"
                      height="32"/>
             </a>
-            <a href={"https://overcast.fm/itunes1269813811/the-life-creative"}>
+            <a href={"https://podcasts.apple.com/us/podcast/compile-swift/id1467065787"}>
                 <img src={require('../img/podcast_players/apple_podcasts.png')} alt="Apple icon" width="32"
                      height="32"/>
             </a>
-            <a href={"https://overcast.fm/itunes1269813811/the-life-creative"}>
+            <a href={"https://open.spotify.com/show/2RGHkl4UtYj0V7lPcRojzK"}>
                 <img src={require('../img/podcast_players/spotify.png')} alt="Spotify icon" width="32" height="32"/>
             </a>
-            <a href={"https://overcast.fm/itunes1269813811/the-life-creative"}>
-                <img src={require('../img/podcast_players/google_play.png')} alt="Google Play icon" width="32"
-                     height="32"/>
-            </a>
-            <a href={"https://overcast.fm/itunes1269813811/the-life-creative"}>
+            <a href={"https://www.google.com/podcasts?feed=aHR0cHM6Ly9hbmNob3IuZm0vcy9iZDg2YjM4L3BvZGNhc3QvcnNz"}>
                 <img src={require('../img/podcast_players/google_podcasts.png')} alt="Google Podcasts icon" width="32"
                      height="32"/>
             </a>
-            <a href={"https://overcast.fm/itunes1269813811/the-life-creative"}>
-                <img src={require('../img/podcast_players/castbox.png')} alt="Castbox player icon" width="32"
-                     height="32"/>
-            </a>
-            <a href={"https://overcast.fm/itunes1269813811/the-life-creative"}>
+            <a href={"https://www.breaker.audio/compile-swift"}>
                 <img src={require('../img/podcast_players/breaker.png')} alt="Breaker player icon" width="32"
                      height="32"/>
             </a>
@@ -48,4 +40,4 @@ const PodcastLinksTLC = () => (
     </div>
 );
 
-export default PodcastLinksTLC
+export default PodcastLinksCS
--- src/pages/podcasts/ggc/ggc-s01-e10-sales.md
+++ src/pages/podcasts/ggc/ggc-s01-e10-sales.md
@@ -13,6 +13,7 @@ We talk about this and using sales to bring back players that may of moved on.
 While you are at it, catch our streams at
 
 [https://twitch.tv/masternewms](https://twitch.tv/masternewms)
+
 [https://twitch.tv/grfxg](https://twitch.tv/grfxg)
 
 <iframe src="https://anchor.fm/gamecode/embed/episodes/Are-sales-really-a-good-thing--We-discuss-both-sides-of-the-argument-e4ml9d" height="102" width="400" frameborder="0" scrolling="no"></iframe>
