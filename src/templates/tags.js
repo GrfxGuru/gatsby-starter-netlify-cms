131bfc0d3f88fc84498c74d11ab3b680c5b6353a
Peter Witham <peter.witham@gmail.com>
2019-10-27 15:42:38 -0500

    Removed is-size3 from tag list

--- src/templates/tags.js
+++ src/templates/tags.js
@@ -1,56 +1,53 @@
-import React from 'react'
-import Helmet from 'react-helmet'
-import { Link, graphql } from 'gatsby'
-import Layout from '../components/Layout'
-import Sidebar from '../components/sidebar'
-import Footer from '../components/footer'
+import React from "react";
+import Helmet from "react-helmet";
+import { Link, graphql } from "gatsby";
+import Layout from "../components/Layout";
+import Sidebar from "../components/sidebar";
+import Footer from "../components/footer";
 
 class TagRoute extends React.Component {
-  render () {
-    const posts = this.props.data.allMarkdownRemark.edges
+  render() {
+    const posts = this.props.data.allMarkdownRemark.edges;
     const postLinks = posts.map(post => (
       <li key={post.node.frontmatter.slug}>
         <Link to={post.node.frontmatter.slug}>
-        <p className='is-size-2'>
-          {post.node.frontmatter.title}
-        </p>
+          <p className="is-size-2">{post.node.frontmatter.title}</p>
         </Link>
       </li>
-    ))
-    const tag = this.props.pageContext.tag
-    const title = this.props.data.site.siteMetadata.title
-    const totalCount = this.props.data.allMarkdownRemark.totalCount
+    ));
+    const tag = this.props.pageContext.tag;
+    const title = this.props.data.site.siteMetadata.title;
+    const totalCount = this.props.data.allMarkdownRemark.totalCount;
     const tagHeader = `${totalCount} post${
-            totalCount === 1 ? '' : 's'
-            } tagged with “${tag}”`
+      totalCount === 1 ? "" : "s"
+    } tagged with “${tag}”`;
 
     return (
       <Layout>
-        <section className='section'>
+        <section className="section">
           <Helmet title={`${tag} | ${title}`} />
-          <div className='container content'>
-            <Sidebar/>
-            <div className='columns taglistPage'>
-              <div className='column' style={{marginBottom: '6rem'}}>
-                <h3 className='is-bold-light'>{tagHeader}</h3>
-                <ul className='taglist is-size-3' style={{fontWeight: 'bold'}}>
+          <div className="container content">
+            <Sidebar />
+            <div className="columns taglistPage">
+              <div className="column" style={{ marginBottom: "6rem" }}>
+                <h3 className="is-bold-light">{tagHeader}</h3>
+                <ul className="taglist" style={{ fontWeight: "bold" }}>
                   {postLinks}
                 </ul>
                 <p>
-                  <Link to='/tags/'> Browse all tags
-                  </Link>
+                  <Link to="/tags/"> Browse all tags</Link>
                 </p>
               </div>
             </div>
           </div>
         </section>
-        <Footer/>
+        <Footer />
       </Layout>
-    )
+    );
   }
 }
 
-export default TagRoute
+export default TagRoute;
 
 export const tagPageQuery = graphql`
   query TagSinglePage($tag: String) {
@@ -78,4 +75,4 @@ export const tagPageQuery = graphql`
       }
     }
   }
-`
+`;
