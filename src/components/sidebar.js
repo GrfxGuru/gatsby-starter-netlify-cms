c2b52e88d024c8d337f7f7d9ec202c18f76c8571
Peter Witham <peter.witham@gmail.com>
2019-12-17 15:51:48 -0600

    Added Dev.to icon to sidebar

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -10,6 +10,7 @@ import mailIcon from '../img/mail.svg'
 import youtubeIcon from '../img/youtube-icon.svg'
 import twitchIcon from '../img/twitch-icon.svg'
 import StickerMule from './StickerMule'
+import devToIcon from '../img/dev.svg'
 
 const Sidebar = () => (
   <div className='sidebar'>
@@ -17,35 +18,32 @@ const Sidebar = () => (
     <h2 class='sidebarWidget'>Navigation</h2>
     <a href={'/tags/swift'}><img src={swiftIcon} height='20px' width='18px' /> Swift Programming</a>
     <br/>
-      <a href={'/podcasts'}><img src={tlcPodcastIcon} height='20px' width='20px'/> Listen to Podcasts</a>
+    <a href={'/podcasts'}><img src={tlcPodcastIcon} height='20px' width='20px' /> Listen to Podcasts</a>
     <br/>
-      <a href={'/tags/jam-stack'}>Learn JAMStack</a>
-      <br/>
-      <a href={'/resources/developer/'}>Developer Resources</a>
-      <br/>
+    <a href={'/tags/jam-stack'}>Learn JAMStack</a>
+    <br/>
+    <a href={'/resources/developer/'}>Developer Resources</a>
+    <br/>
     <br/>
     <h2 class='sidebarWidget'>Social</h2>
     <a href={'/contact/'}><img src={mailIcon} width='20px' height='20px' /> Contact Me</a>
     <br/>
-      <a href={'https://twitter.com/compileswift'} target='_blank' rel='noopener noreferrer'>
-          <img src={twitterIcon} width='20px' height='20px'/> Twitter</a>
-      <br/>
-      <a href={'https://www.linkedin.com/in/peterwitham'} target='_blank' rel='noopener noreferrer'>
-          <img src={linkedInIcon} width='20px' height='20px'/> LinkedIN</a>
+    <a href={'https://twitter.com/compileswift'} target='_blank' rel='noopener noreferrer'><img src={twitterIcon} width='20px' height='20px' /> Twitter</a>
     <br/>
-      <a href={'https://github.com/GrfxGuru'} target='_blank' rel='noopener noreferrer'>
-          <img src={githubIcon} width='20px' height='20px'/> GitHub</a>
+    <a href={'https://www.linkedin.com/in/peterwitham'} target='_blank' rel='noopener noreferrer'><img src={linkedInIcon} width='20px' height='20px' /> LinkedIN</a>
     <br/>
-      <a href={'https://youtube.com/grfxg'} target='_blank' rel='noopener noreferrer'>
-          <img src={youtubeIcon} width='20px' height='20px'/> YouTube</a>
-      <br/>
-      <a href={'https://twitch.tv/grfxg'} target='_blank' rel='noopener noreferrer'>
-          <img src={twitchIcon} width='20px' height='20px'/> Twitch.tv</a>
-      <br/>
+    <a href={'https://github.com/GrfxGuru'} target='_blank' rel='noopener noreferrer'><img src={githubIcon} width='20px' height='20px' /> GitHub</a>
+    <br/>
+    <a href={'https://youtube.com/grfxg'} target='_blank' rel='noopener noreferrer'><img src={youtubeIcon} width='20px' height='20px' /> YouTube</a>
+    <br/>
+    <a href={'https://twitch.tv/grfxg'} target='_blank' rel='noopener noreferrer'><img src={twitchIcon} width='20px' height='20px' /> Twitch.tv</a>
+    <br/>
+    <a href={'https://dev.to/peterwitham'} target='_blank' rel='noopener noreferrer'><img src={devToIcon} width='20px' height='20px' /> Dev.to</a>
+    <br/>
     <br/>
     <BuyMeACoffee/>
     <StickerMule/>
   </div>
-);
+)
 
 export default Sidebar
