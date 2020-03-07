d917f2ca1e7a0efaa50dbf33280b70c2353daa13
Peter Witham <peter.witham@gmail.com>
2020-03-07 13:28:03 -0600

    Updated podcast player panels

--- src/components/podcast_cs_links.js
+++ src/components/podcast_cs_links.js
@@ -1,43 +1,102 @@
-import React from 'react'
+import React from "react";
 
 const PodcastLinksCS = () => (
-    <div className="podcastPlayersLinkTable">
-        <h2>Subscribe to the Podcast</h2>
-        <p>If you like this episode, please check out others at <a href="https://pw.d.pr/5TbjRs" target="_blank"
-                                                                   rel="noopener noreferrer">CompileSwift Podcast</a>.
-            Or click the link to your favorite Podcast player.</p>
-        <div className="podcastPlayerIconList">
-            <a href={"https://overcast.fm/itunes1467065787/compile-swift"}>
-                <img src={require('../img/podcast_players/overcast.png')} alt="Overcast icon" width="32" height="32"/>
-            </a>
-            <a href={"https://pw.d.pr/5TbjRs"}>
-                <img src={require('../img/podcast_players/anchor.png')} alt="Anchor icon" width="32" height="32"/>
-            </a>
-            <a href={"http://pca.st/uEB6"}>
-                <img src={require('../img/podcast_players/pocket_casts.png')} alt="Pocket Casts icon" width="32"
-                     height="32"/>
-            </a>
-            <a href={"https://radiopublic.com/compile-swift-G7VbpB"}>
-                <img src={require('../img/podcast_players/radiopublic.png')} alt="Radio Public icon" width="32"
-                     height="32"/>
-            </a>
-            <a href={"https://podcasts.apple.com/us/podcast/compile-swift/id1467065787"}>
-                <img src={require('../img/podcast_players/apple_podcasts.png')} alt="Apple icon" width="32"
-                     height="32"/>
-            </a>
-            <a href={"https://open.spotify.com/show/2RGHkl4UtYj0V7lPcRojzK"}>
-                <img src={require('../img/podcast_players/spotify.png')} alt="Spotify icon" width="32" height="32"/>
-            </a>
-            <a href={"https://www.google.com/podcasts?feed=aHR0cHM6Ly9hbmNob3IuZm0vcy9iZDg2YjM4L3BvZGNhc3QvcnNz"}>
-                <img src={require('../img/podcast_players/google_podcasts.png')} alt="Google Podcasts icon" width="32"
-                     height="32"/>
-            </a>
-            <a href={"https://www.breaker.audio/compile-swift"}>
-                <img src={require('../img/podcast_players/breaker.png')} alt="Breaker player icon" width="32"
-                     height="32"/>
-            </a>
-        </div>
+  <div className="podcastPlayersLinkTable">
+    <h2>Subscribe to the Podcast</h2>
+    <p>
+      If you like this episode, please check out others at{" "}
+      <a
+        href="https://pw.d.pr/5TbjRs"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        CompileSwift Podcast
+      </a>
+      . Or click the link to your favorite Podcast player.
+    </p>
+    <div className="podcastPlayerIconList">
+      <div className={"podcastPlayerIcon"}>
+        <a href={"https://overcast.fm/itunes1467065787/compile-swift"}>
+          <img
+            src={require("../img/podcast_players/overcast.png")}
+            alt="Overcast icon"
+            width="32"
+            height="32"
+          />
+          <br />
+          <b>Overcast</b>
+        </a>
+      </div>
+      <div className={"podcastPlayerIcon"}>
+        <a
+          href={
+            "https://podcasts.apple.com/us/podcast/compile-swift/id1467065787"
+          }
+        >
+          <img
+            src={require("../img/podcast_players/apple_podcasts.png")}
+            alt="Apple icon"
+            width="32"
+            height="32"
+          />
+          <br />
+          <b>Apple</b>
+        </a>
+      </div>
+      <div className={"podcastPlayerIcon"}>
+        <a href={"https://open.spotify.com/show/2RGHkl4UtYj0V7lPcRojzK"}>
+          <img
+            src={require("../img/podcast_players/spotify.png")}
+            alt="Spotify icon"
+            width="32"
+            height="32"
+          />
+          <br />
+          <b>Spotify</b>
+        </a>
+      </div>
+      <div className={"podcastPlayerIcon"}>
+        <a
+          href={
+            "https://www.google.com/podcasts?feed=aHR0cHM6Ly9hbmNob3IuZm0vcy9iZDg2YjM4L3BvZGNhc3QvcnNz"
+          }
+        >
+          <img
+            src={require("../img/podcast_players/google_podcasts.png")}
+            alt="Google Podcasts icon"
+            width="32"
+            height="32"
+          />
+          <br />
+          <b>Google</b>
+        </a>
+      </div>
+      <div className={"podcastPlayerIcon"}>
+        <a href={"http://pca.st/uEB6"}>
+          <img
+            src={require("../img/podcast_players/pocket_casts.png")}
+            alt="Pocket Casts icon"
+            width="32"
+            height="32"
+          />
+          <br />
+          <b>Pocketcasts</b>
+        </a>
+      </div>
+      <div className={"podcastPlayerIcon"}>
+        <a href={"https://pw.d.pr/5TbjRs"}>
+          <img
+            src={require("../img/podcast_players/anchor.png")}
+            alt="Anchor icon"
+            width="32"
+            height="32"
+          />
+          <br />
+          <b>All the links</b>
+        </a>
+      </div>
     </div>
+  </div>
 );
 
-export default PodcastLinksCS
+export default PodcastLinksCS;
--- src/components/podcast_tlc_links.js
+++ src/components/podcast_tlc_links.js
@@ -1,51 +1,103 @@
-import React from 'react'
+import React from "react";
 
 const PodcastLinksTLC = () => (
-    <div className="podcastPlayersLinkTable">
-        <h2>Subscribe to the Podcast</h2>
-        <p>If you like this episode, please check out others at <a href="https://pw.d.pr/NBc0dv" target="_blank"
-                                                                   rel="noopener noreferrer"> The Life Creative
-            Podcast</a>. Or click the link to your favorite Podcast player.</p>
-        <div className="podcastPlayerIconList">
-            <a href={"https://overcast.fm/itunes1269813811/the-life-creative"}>
-                <img src={require('../img/podcast_players/overcast.png')} alt="Overcast icon" width="32" height="32"/>
-            </a>
-            <a href={"https://pw.d.pr/NBc0dv"}>
-                <img src={require('../img/podcast_players/anchor.png')} alt="Anchor icon" width="32" height="32"/>
-            </a>
-            <a href={"http://pca.st/uEB6"}>
-                <img src={require('../img/podcast_players/pocket_casts.png')} alt="Pocket Casts icon" width="32"
-                     height="32"/>
-            </a>
-            <a href={"https://play.radiopublic.com/the-life-creative-WYBnel"}>
-                <img src={require('../img/podcast_players/radiopublic.png')} alt="Radio Public icon" width="32"
-                     height="32"/>
-            </a>
-            <a href={"https://podcasts.apple.com/us/podcast/the-life-creative/id1269813811"}>
-                <img src={require('../img/podcast_players/apple_podcasts.png')} alt="Apple icon" width="32"
-                     height="32"/>
-            </a>
-            <a href={"https://open.spotify.com/show/1hd27peL8JEoy4WMJq33Un"}>
-                <img src={require('../img/podcast_players/spotify.png')} alt="Spotify icon" width="32" height="32"/>
-            </a>
-            <a href={"https://playmusic.app.goo.gl/?ibi=com.google.PlayMusic&isi=691797987&ius=googleplaymusic&link=https://play.google.com/music/m/Idhzktukhazvz55gayca5mdfocy?t%3DThe_Life_Creative%26pcampaignid%3DMKT-na-a"}>
-                <img src={require('../img/podcast_players/google_play.png')} alt="Google Play icon" width="32"
-                     height="32"/>
-            </a>
-            <a href={"https://www.google.com/podcasts?feed=aHR0cHM6Ly9hbmNob3IuZm0vcy85YTJmMTgvcG9kY2FzdC9yc3M="}>
-                <img src={require('../img/podcast_players/google_podcasts.png')} alt="Google Podcasts icon" width="32"
-                     height="32"/>
-            </a>
-            <a href={"https://castbox.fm/channel/id1015404"}>
-                <img src={require('../img/podcast_players/castbox.png')} alt="Castbox player icon" width="32"
-                     height="32"/>
-            </a>
-            <a href={"https://www.breaker.audio/the-life-creative"}>
-                <img src={require('../img/podcast_players/breaker.png')} alt="Breaker player icon" width="32"
-                     height="32"/>
-            </a>
-        </div>
+  <div className="podcastPlayersLinkTable">
+    <h2>Subscribe to the Podcast</h2>
+    <p>
+      If you like this episode, please check out others at{" "}
+      <a
+        href="https://pw.d.pr/NBc0dv"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {" "}
+        The Life Creative Podcast
+      </a>
+      . Or click the link to your favorite Podcast player.
+    </p>
+    <div className="podcastPlayerIconList">
+      <div className={"podcastPlayerIcon"}>
+        <a href={"https://overcast.fm/itunes1269813811/the-life-creative"}>
+          <img
+            src={require("../img/podcast_players/overcast.png")}
+            alt="Overcast icon"
+            width="32"
+            height="32"
+          />
+          <br />
+          <b>Overcast</b>
+        </a>
+      </div>
+      <div className={"podcastPlayerIcon"}>
+        <a
+          href={
+            "https://podcasts.apple.com/us/podcast/the-life-creative/id1269813811"
+          }
+        >
+          <img
+            src={require("../img/podcast_players/apple_podcasts.png")}
+            alt="Apple icon"
+            width="32"
+            height="32"
+          />
+          <br />
+          <b>Apple</b>
+        </a>
+      </div>
+      <div className={"podcastPlayerIcon"}>
+        <a href={"https://open.spotify.com/show/1hd27peL8JEoy4WMJq33Un"}>
+          <img
+            src={require("../img/podcast_players/spotify.png")}
+            alt="Spotify icon"
+            width="32"
+            height="32"
+          />
+          <br />
+          <b>Spotify</b>
+        </a>
+      </div>
+      <div className={"podcastPlayerIcon"}>
+        <a
+          href={
+            "https://www.google.com/podcasts?feed=aHR0cHM6Ly9hbmNob3IuZm0vcy85YTJmMTgvcG9kY2FzdC9yc3M="
+          }
+        >
+          <img
+            src={require("../img/podcast_players/google_podcasts.png")}
+            alt="Google Podcasts icon"
+            width="32"
+            height="32"
+          />
+          <br />
+          <b>Google</b>
+        </a>
+      </div>
+      <div className={"podcastPlayerIcon"}>
+        <a href={"http://pca.st/uEB6"}>
+          <img
+            src={require("../img/podcast_players/pocket_casts.png")}
+            alt="Pocket Casts icon"
+            width="32"
+            height="32"
+          />
+          <br />
+          <b>Pocketcasts</b>
+        </a>
+      </div>
+      <div className={"podcastPlayerIcon"}>
+        <a href={"https://pw.d.pr/NBc0dv"}>
+          <img
+            src={require("../img/podcast_players/anchor.png")}
+            alt="Anchor icon"
+            width="32"
+            height="32"
+          />
+          <br />
+          <b>All the links</b>
+        </a>
+      </div>
     </div>
+  </div>
 );
 
-export default PodcastLinksTLC
+export default PodcastLinksTLC;
